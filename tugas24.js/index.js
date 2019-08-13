f8ba2eb7d16ad66722027b87a0eabe23fb17c183
Engkos <engkoskostaman97@gmail.com>
2019-08-13 09:24:00 +0700

    tugas 24 js
    tugas ini kami kumpulkan untuk niomic

---
+++ tugas24.js/index.js
@@ -0,0 +1,9 @@
+function panggilsort(){
+  var angka=[32,33,16,40,9,7,11,2,44,1,66];
+
+  console.log("sebelumnya: " + angka )
+	console.log("Ascending: " + angka.sort())
+	console.log("Descending: " + angka.reverse())
+
+}
+panggilsort();
